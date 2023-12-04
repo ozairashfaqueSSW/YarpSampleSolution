c9a34fbef0fbecb63e855b3d7a216f5dacf97301
Ozair Ashfaque <ozairashfaque@ssw.com.au>
2023-12-04 19:03:10 +1100

    Removed webapp from side-by-side increment miragation branch

--- src/WebApp/Model/DataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApp.Model
-{
-    public class DataModel
-    {
-        public string Source { get; set; } = string.Empty;
-
-        public string Data { get; set; } = string.Empty;
-    }
-}
