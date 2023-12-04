151b84262973297f84b1e35f16766cbc6ca0cb3d
Ozair Ashfaque <ozairashfaque@ssw.com.au>
2023-12-04 18:45:20 +1100

    Added ApplicationInformationController

---
+++ src/Yarp.Gateway/Controllers/ApplicationInformationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yarp.Gateway.Controllers;
+
+[Route("webapp")]
+[ApiController]
+public class ApplicationInformationController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { source = "WebApp .NET 8", data = "WebApp .NET 8 Data" });
+    }
+}
